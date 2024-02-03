83177914e0e02dc3b4e3cf8c84edd926e1325dcd
Abhinav <jha.abhinav.anand12@gmail.com>
2024-02-03 23:45:35 +0530

    server running

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+import userRoute from '../routes/user.routes.js'
+
+app.use("/users",userRoute)
+
 export {app}
