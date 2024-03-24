2c325dd550afa21473a47ed2d282f809e5dac6a2
Abhinav <jha.abhinav.anand12@gmail.com>
2024-03-24 17:57:44 +0530

    subscription  controllers

--- controllers/subscription.controllers.js
+++ controllers/subscription.controllers.js
@@ -10,77 +10,95 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 //check again
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-    const channel_id = await User.findOne({
-        $or: [{channelId}]
-    })
-    if (!channel_id) {
+    if (!isValidObjectId(channelId)) {
         throw new ApiError(404, "channel does not exist")
     }
-    const user = req.user.username
-    const subscribe_doc = await Subscription.create({
-        user,
-        channel_id
-    })
-    if(subscribe_doc){
-        return res
-            .status(200)
-            .json(new ApiResponse(200,_,"subscribed successfully"))
-    }
-    else{
-        throw new ApiError(409, "Not able to subscribe, try never again")
-    }
    
+    //check if already subscribed
+    // const channel = await User.find({_id:channelId})
+
+    const subscriptionDoc = await Subscription.find({subscriber : req.user._id, channel: channel._id})
+    if(subscriptionDoc){
+        // unsubscribe
+        const result = await Subscription.findByIdAndDelete(subscriptionDoc._id);
+        return res
+                .status(201)
+                .json(
+                    new ApiResponse(201,{},"Unsubscribed Successfully")
+                )
+    }
+
+    await Subscription.create({
+        subscriber: req.user?._id,
+        channel: channelId,
+    })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "subscribed successfully"
+            )
+        )
+
+    
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-    if(!channelId?.trim()){
-        throw new ApiError(400, "username is missing")
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "invalid userid")
     }
-    const subscribers = await Subscription.aggregate(
-        [
-            {
-                $match: {
-                    username: channelId?.toLowerCase()
-                }
-            }
-        ]
-    )
-    if (!subscribers?.length) {
-        throw new ApiError(404, "channel does not exists")
-    }
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel:   new mongoose.Types.ObjectId(channelId)
+            },
+            $lookup:{
+                from:"users",
+                localField:"subscriber",
+                foreignField:"_id",
+                as:"subscribers"
+            },
+        }
+    ])
+    
 
     return res
     .status(200)
     .json(
-        new ApiResponse(200, subscribers[0], "Subscribers channel fetched successfully")
+        new ApiResponse(200, subscribers.subscribers, "Subscribers channel fetched successfully")
     )
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-    if(!subscriberId?.trim()){
-        throw new ApiError(400, "username is missing")
+    const {channelId} = req.params
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "invalid userid")
     }
-    const Channels = await Subscription.aggregate(
-        [
-            {
-                $match: {
-                    username: subscriberId?.toLowerCase()
-                }
-            }
-        ]
-    )
-    if (!Channels?.length) {
-        throw new ApiError(404, "channel does not exists")
-    }
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber:   new mongoose.Types.ObjectId(channelId)
+            },
+            $lookup:{
+                from:"users",
+                localField:"subscriber",
+                foreignField:"_id",
+                as:"channels"
+            },
+        }
+    ])
+    
 
     return res
     .status(200)
     .json(
-        new ApiResponse(200, Channels[0], "Subscribers channel fetched successfully")
+        new ApiResponse(200, subscribers.channels, "Subscribed channels fetched successfully")
     )
 })
 
